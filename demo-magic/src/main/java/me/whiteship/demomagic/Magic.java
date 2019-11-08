e657ce9a00d0f198b766c65fe29d8527e76f9ad9
Keesun Baik <whiteship2000@gmail.com>
2019-11-07 23:00:37 -0800

    4-Pull-Out-Rabbit

--- demo-magic/src/main/java/me/whiteship/demomagic/Magic.java
+++ demo-magic/src/main/java/me/whiteship/demomagic/Magic.java
@@ -1,10 +1,25 @@
 package me.whiteship.demomagic;
 
 import me.whiteship.demomojalib.Box;
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
+import net.bytebuddy.dynamic.loading.ClassReloadingStrategy;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.pool.TypePool;
+
+import static net.bytebuddy.matcher.ElementMatchers.named;
 
 public class Magic {
 
     public static void main(String[] args) {
+        ByteBuddyAgent.install();
+        new ByteBuddy().redefine(Box.class)
+                .method(named("open"))
+                .intercept(FixedValue.value("Rabbit"))
+                .make()
+                .load(Box.class.getClassLoader(), ClassReloadingStrategy.fromInstalledAgent());
+
         Box box = new Box();
         System.out.println(box.open());
     }
