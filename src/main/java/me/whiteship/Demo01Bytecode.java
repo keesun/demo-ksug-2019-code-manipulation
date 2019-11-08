f27d162260544247b7c105e150d568ed4d5a0601
Keesun Baik <whiteship2000@gmail.com>
2019-11-07 21:51:09 -0800

    1. bytecode

---
+++ src/main/java/me/whiteship/Demo01Bytecode.java
@@ -0,0 +1,18 @@
+package me.whiteship;
+
+public class Demo01Bytecode {
+
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello KSUG 2019");
+    }
+}
