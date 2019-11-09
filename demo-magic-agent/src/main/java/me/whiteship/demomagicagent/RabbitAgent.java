6a2f19d1eb5a7f429793a95ee7497b2111b3fcb2
Keesun Baik <whiteship2000@gmail.com>
2019-11-08 17:18:47 -0800

    JavaAgent completed

--- demo-magic-agent/src/main/java/me/whiteship/demomagicagent/RabbitAgent.java
+++ demo-magic-agent/src/main/java/me/whiteship/demomagicagent/RabbitAgent.java
@@ -1,20 +1,23 @@
 package me.whiteship.demomagicagent;
 
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.lang.instrument.Instrumentation;
 import java.security.ProtectionDomain;
 
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
 public class RabbitAgent {
 
     public static void premain(String args, Instrumentation instrumentation) {
-        instrumentation.addTransformer(new ClassFileTransformer() {
-            @Override
-            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
-                System.out.println("Transform :" + className);
-                return new byte[0];
-            }
-        });
+        new AgentBuilder.Default()
+                .type(named("me.whiteship.demomojalib.Box"))
+                .transform(((builder, typeDescription, classLoader, module) ->
+                    builder.method(named("open")).intercept(FixedValue.value("Rabbit")))).installOn(instrumentation);
     }
 
 }
