c4aa43fb1f8b3be82f2714126b0b2b3f8f73ccae
Keesun Baik <whiteship2000@gmail.com>
2019-11-08 17:11:55 -0800

    JavaAgent Begin

---
+++ demo-magic-agent/src/main/java/me/whiteship/demomagicagent/RabbitAgent.java
@@ -0,0 +1,20 @@
+package me.whiteship.demomagicagent;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.security.ProtectionDomain;
+
+public class RabbitAgent {
+
+    public static void premain(String args, Instrumentation instrumentation) {
+        instrumentation.addTransformer(new ClassFileTransformer() {
+            @Override
+            public byte[] transform(ClassLoader loader, String className, Class<?> classBeingRedefined, ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
+                System.out.println("Transform :" + className);
+                return new byte[0];
+            }
+        });
+    }
+
+}
